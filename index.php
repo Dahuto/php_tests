ff0332945d5b4385434ba4696e41825f7bc59a9d
Darja Hutorovskaja <dh@Darjas-MacBook-Air.local>
2021-08-24 18:41:59 +0300

    New

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
-<?php
-gmp_testbit()
-echo 'test';
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>My first php page</title>
+    </head>
+    <body>
+        <?php
+            echo "Hello world"
+        ?>
+    </body>
+</html>
